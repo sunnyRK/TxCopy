bc48b6e5abdb2957236cd76cb30a744390c710f1
sunnyRK <radadiyasunny970@gmail.com>
2023-04-24 20:02:52 +0530

    new changes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Image from 'next/image';
 
 export default function Document() {
     return (
         <Html lang="en">
             <script src="https://cdn.tailwindcss.com"></script>
+            <title>DefiLens</title>
+            <link rel = "icon" href ="/logo.png" type="image/x-icon"></link>
             <Head />
             <body>
                 <Main />
