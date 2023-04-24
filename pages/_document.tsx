7267f4cd37e181f485638ad1c3feae005b6a86ed
sunnyRK <radadiyasunny970@gmail.com>
2023-04-24 11:38:02 +0530

    tailwind script added

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,11 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
     return (
         <Html lang="en">
+            <script src="https://cdn.tailwindcss.com"></script>
             <Head />
             <body>
                 <Main />
                 <NextScript />
-                <script src="https://cdn.tailwindcss.com"></script>
             </body>
         </Html>
     );
