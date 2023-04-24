402d2080311b8aa2bdeb6b16f566de309c09b0ce
sunnyRK <radadiyasunny970@gmail.com>
2023-04-24 11:34:24 +0530

    update with tailwind css

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,7 @@ export default function Document() {
             <body>
                 <Main />
                 <NextScript />
+                <script src="https://cdn.tailwindcss.com"></script>
             </body>
         </Html>
     );
