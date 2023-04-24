b231a559662c8c64c37d984f388a5800ddf77b33
sunnyRK <radadiyasunny970@gmail.com>
2023-04-24 11:05:39 +0530

    minox fixes

--- Components/Footer.tsx
+++ Components/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
         <div className="w-full bg-[#8a46ff] py-2">
             <div className="container mx-auto">
                 <a href="https://twitter.com/RadadiyaSunny" target="_blank">
-                    sunnyrk.eth
+                    Twitter: sunnyrk.eth
                 </a>
             </div>
         </div>
